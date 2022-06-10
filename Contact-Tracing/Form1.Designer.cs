386666c941a66319da6f5dfd2794c21ba9a8c850
Joshua Sundiam <104367499+JoshuaSundiam@users.noreply.github.com>
2022-06-10 23:42:52 +0800

    coding

--- Contact-Tracing/Form1.Designer.cs
+++ Contact-Tracing/Form1.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbox1 = new System.Windows.Forms.TextBox();
+            this.tbox2 = new System.Windows.Forms.TextBox();
+            this.tbox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(240, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(163, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contact No.:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(204, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address:";
+            // 
+            // tbox1
+            // 
+            this.tbox1.Location = new System.Drawing.Point(336, 96);
+            this.tbox1.Name = "tbox1";
+            this.tbox1.Size = new System.Drawing.Size(215, 27);
+            this.tbox1.TabIndex = 3;
+            // 
+            // tbox2
+            // 
+            this.tbox2.Location = new System.Drawing.Point(336, 161);
+            this.tbox2.Name = "tbox2";
+            this.tbox2.Size = new System.Drawing.Size(215, 27);
+            this.tbox2.TabIndex = 4;
+            // 
+            // tbox3
+            // 
+            this.tbox3.Location = new System.Drawing.Point(336, 223);
+            this.tbox3.Name = "tbox3";
+            this.tbox3.Size = new System.Drawing.Size(215, 27);
+            this.tbox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(323, 337);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(209, 67);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Contact_Tracing.Properties.Resources.Screenshot_2022_06_10_221414;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbox3);
+            this.Controls.Add(this.tbox2);
+            this.Controls.Add(this.tbox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbox1;
+        private TextBox tbox2;
+        private TextBox tbox3;
+        private Button button1;
     }
 }
--- Contact-Tracing/Form1.cs
+++ Contact-Tracing/Form1.cs
@@ -6,5 +6,18 @@ namespace Contact_Tracing
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter folder = new StreamWriter(@"C:\Users\JOSHUA\Desktop\Contact Tracing\Info.txt", true);
+
+            folder.WriteLine("Name:" + tbox1);
+            folder.WriteLine("Contact No.:" + tbox2);
+            folder.WriteLine("Address:" + tbox3);
+            
+
+            
+
+        }
     }
 }
